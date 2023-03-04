11a5a01267f15594a62442cd817b377744f4089e
Luka Bergant <luka@bergant.com>
2023-03-04 14:19:38 +0100

    Added tag prints and formatted other prints

--- web/js/index.js
+++ web/js/index.js
@@ -19,28 +19,39 @@ function assemble() {
 	let asm = new Assembler();
 	asm.main(lines);
 
+	print_assembler_result(asm);
+	print_tags();
+}
+
+function print_tags() {
+	let tag_text = "";
+	let tag_pad = "          ";
+
+	for (let i = 0; i < tags.length; i++) {
+		tag_text += tags[i].name.padEnd(10) + " @ " + tags[i].address + "\n";
+	}
+
+	$("#taTag").val(tag_text);
+}
+
+function print_assembler_result(asm) {
 	let ram_text = "";
-	let pad_addr = "0000";
-	let pad = "00";
 
 	for (let i = 0; i < asm.commands.length; i++) {
 		// add address and command to line
 		ram_text +=
-			(pad_addr + asm.commands[i].address.toString(16).toUpperCase()).slice(
-				-pad_addr.length
-			) +
+			asm.commands[i].address.toString(16).toUpperCase().padStart(4, "0") +
 			": " +
-			(pad + asm.commands[i].op_code.toString(16).toUpperCase()).slice(
-				-pad.length
-			);
+			asm.commands[i].op_code.toString(16).toUpperCase().padStart(2, "0");
 
 		// add operands to line
 		for (let j = 0; j < asm.commands[i].operands.length; j++) {
 			ram_text +=
 				" " +
-				(
-					pad + asm.commands[i].operands[j].value.toString(16).toUpperCase()
-				).slice(-pad.length);
+				asm.commands[i].operands[j].value
+					.toString(16)
+					.toUpperCase()
+					.padStart(2, "0");
 		}
 
 		// left pad for shorter instructions
